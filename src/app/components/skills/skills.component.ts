a33d87e76d0da3a5ec80032f4a4380953b209fcf
albano.fuentesmaya.contractor.gdevtools.com <albano.fuentesmaya.contractor.gdevtools.com@bbva.com>
2020-06-06 04:16:17 -0300

    3era noche de codeo

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  skillLevelLow = 'assets/img/barra-1-4.png';
+  skillLevelMedium = 'assets/img/barra1-2.png';
+  skillLevelHigh = 'assets/img/barra-3-4.png';
+  skillLevelVeryHigh = 'assets/img/barra-full.png';
   constructor() { }
 
   ngOnInit(): void {
